1a79fc824208557449ba97709bb666ae80bbd759
Alex <alex@stayopen.com>
2021-10-26 22:22:18 -0700

    adds simple streamlit app that pings GPT-J endpoint and helps unblock writers

---
+++ writers/unblock.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+size = st.sidebar.selectbox(
+    'How long a response do you want?',
+    ('Tweet Length', 'Paragraph', 'Page')
+)
+
+st.write(size)
+
+lmaps = {'Tweet Length': 120, 'Paragraph': 320, 'Page': 1000}
+
+context = st.text_input('Write your best opening paragraph here')
+
+@st.cache
+def generate_text(context):
+    payload = {
+        "context": context,
+        "token_max_length": lmaps[size],
+        "temperature": 1.0,
+        "top_p": 0.9,
+    }
+    response = requests.post("http://api.vicgalle.net:5000/generate", params=payload).json()
+    return response
+
+response = generate_text(context)
+
+#st.subheader('Here we go!')
+st.caption(context)
+st.write(response['text'])
+
+st.balloons()
